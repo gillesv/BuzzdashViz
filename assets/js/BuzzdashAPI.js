633d98aa57dfceb661cb52f8e5cffebd46870fe4
Gilles Vandenoostende <gilles.vandenoostende@duvalguillaume.com>
2014-07-17 10:35:59 +0200

    initial import & commit

---
+++ assets/js/BuzzdashAPI.js
@@ -0,0 +1,332 @@
+function BuzzdashAPI() {
+	this.campaignLoaded = function(data){};
+}
+
+BuzzdashAPI.prototype.loadCampaign = function(campaignID) {
+
+	
+	
+	var blog_date_date = Array();
+	var blog_total_mentions = Array();
+	var blog_date_date = Array();
+	var blog_total_tweets = Array();
+	var blog_total_fblikes = Array();
+	var blog_total_fbshares = Array()
+	var blog_total_fbcomments = Array();
+	var blog_total_fbclicks = Array(); 
+	var blog_total_mentions = Array(); 
+	var blog_total_share = Array();
+
+	var video_date_date = Array();
+	var video_total_tweets = Array(); 
+	var video_total_fblikes = Array(); 
+	var video_total_fbshares = Array(); 
+	var video_total_fbcomments = Array(); 
+	var video_total_fbclicks = Array(); 
+	var video_total_mentions = Array(); 
+	var video_total_ytviews = Array(); 
+	var video_total_ytcomments = Array(); 
+	var video_total_share	= Array();
+
+	var sources = Array();
+	sources["this"] = this;
+	sources["blog_date_date"] = blog_date_date;
+	sources["blog_total_mentions"] = blog_total_mentions;
+	sources["blog_date_date"] = blog_date_date;
+	sources["blog_total_tweets"] = blog_total_tweets;
+	sources["blog_total_fblikes"] = blog_total_fblikes;
+	sources["blog_total_fbshares"] = blog_total_fbshares;
+	sources["blog_total_fbcomments"] = blog_total_fbcomments;
+	sources["blog_total_fbclicks"] = blog_total_fbclicks;
+	sources["blog_total_mentions"] = blog_total_mentions;
+	sources["blog_total_share"] = blog_total_share;
+	sources["video_date_date"] = video_date_date;
+	sources["video_total_tweets"] = video_total_tweets;
+	sources["video_total_fblikes"] = video_total_fblikes;
+	sources["video_total_fbshares"] = video_total_fbshares;
+	sources["video_total_fbcomments"] = video_total_fbcomments;
+	sources["video_total_fbclicks"] = video_total_fbclicks;
+	sources["video_total_mentions"] = video_total_mentions;
+	sources["video_total_ytviews"] = video_total_ytviews;
+	sources["video_total_ytcomments"] = video_total_ytcomments;
+	sources["video_total_share"] = video_total_share;
+
+
+
+
+	  $.ajax({
+		type: "POST",
+		dataType: "json",
+		url: "http://buzzdash.duvalguillaume.com/api/getgraphdata?cache=" + Math.random(),
+		data: { id: campaignID }
+	  }).done(function ( data ) {
+
+	  var maindata = Array();
+
+	  jQuery.each(data.blogmentions.statsintime, function() {
+		if( parseInt(this.mentions) - parseInt(blog_total_mentions[blog_total_mentions.length-1]) >= -10 || blog_total_mentions.length == 0 || 1 == 1){
+		  blog_date_date.push(this.date_date);
+		  blog_total_tweets.push(this.tweets);
+		  blog_total_fblikes.push(this.fblikes);
+		  blog_total_fbshares.push(this.fbshares);
+		  blog_total_fbcomments.push(this.fbcomments);
+		  blog_total_fbclicks.push(this.fbclicks);
+		  blog_total_mentions.push(this.mentions);
+		  blog_total_share.push(parseInt(this.tweets) + parseInt(this.fblikes) + parseInt(this.fbshares) + parseInt(this.fbcomments));
+
+		} 
+	  });
+
+	  jQuery.each(data.videomentions.statsintime, function() {
+		if(	 parseInt(this.mentions) - parseInt(video_total_mentions[video_total_mentions.length-1]) >= -10 || video_total_mentions.length == 0){
+		video_date_date.push(this.date_date);
+		video_total_tweets.push(this.tweets);
+		video_total_fblikes.push(this.fblikes);
+		video_total_fbshares.push(this.fbshares);
+		video_total_fbcomments.push(this.fbcomments);
+		video_total_fbclicks.push(this.fbclicks);
+		video_total_mentions.push(this.mentions);
+		video_total_ytviews.push(this.views);
+		video_total_ytcomments.push(this.ytcomments);
+		video_total_share.push(parseInt(this.tweets) + parseInt(this.fblikes) + parseInt(this.fbshares) + parseInt(this.fbcomments));
+
+		}
+	  });
+
+	
+	  date_date = union_arrays (video_date_date, blog_date_date);
+	  date_date.sort();
+
+  
+
+  var datestouse
+  datestouse = date_date
+
+  var numDataLines = 0;
+  
+	var firstdatetouse = datestouse[0];
+	var firstdatetousearr = firstdatetouse.split("-");
+	var firstdate = new Date(parseInt(firstdatetousearr[0]), parseInt(firstdatetousearr[1])-1, parseInt(firstdatetousearr[2]));
+
+	var lastdatetouse = datestouse[datestouse.length-1];
+	var lastdatetousearr = lastdatetouse.split("-");
+	var lastdate = new Date(parseInt(lastdatetousearr[0]), parseInt(lastdatetousearr[1])-1, parseInt(lastdatetousearr[2]));
+  
+  lastdate.setDate(lastdate.getDate()+1);
+
+  var daycounter = 0;
+  var currentdate = firstdate
+  var max_mentions = 0;
+  var previousShares = 0;
+  var previousViews = 0;
+
+  var dayswithoutdata = 1;
+
+  var maxShares = 0;
+  var maxViews = 0;
+
+  while (currentdate.toString() != lastdate.toString())
+  {
+	
+	
+	var curr_date = currentdate.getDate();
+	var curr_month = currentdate.getMonth() + 1; //Months are zero based
+	var curr_year = currentdate.getFullYear();
+	var curr_date_format = '' + curr_year +'-'+ (curr_month<=9?'0'+curr_month:curr_month) +'-'+ (curr_date<=9?'0'+curr_date:curr_date);
+	var i = datestouse.indexOf(curr_date_format);
+	
+
+	if( i == -1 ){	
+	
+	  var datarow = Array();
+	  datarow["date"] = (curr_date_format);
+	  //datarow = addStatsForType(datarow, 'ytviews', null, sources);
+	  //datarow = addStatsForType(datarow, 'ytcomments', null, sources);
+	  //datarow = addStatsForType(datarow, 'tweets', null, sources);
+	  //datarow = addStatsForType(datarow, 'fblikes', null, sources);
+	  //datarow = addStatsForType(datarow, 'fbshares', null, sources);
+	  //datarow = addStatsForType(datarow, 'fbcomments', null, sources);
+	  //datarow = addStatsForType(datarow, 'shareratio', null, sources);
+	  datarow["total_shares"] = null;
+ 		datarow["num_shares"] = null;	   
+ 		datarow["estimated"] = false;
+ 		datarow["total_views"] = null; 
+ 		datarow["num_views"] = null;
+	  //datarow.push("test");
+	  numDataLines = datarow.length;
+	  maindata.push(datarow);
+	  dayswithoutdata++;
+	  
+	} else {
+	  
+	  var datarow = Array();
+	  datarow["date"] = (datestouse[i]);
+	  //datarow = addStatsForType(datarow, 'ytviews', datestouse[i], sources);
+	  //datarow = addStatsForType(datarow, 'ytcomments', datestouse[i], sources);
+	  //datarow = addStatsForType(datarow, 'tweets', datestouse[i], sources);
+	  //datarow = addStatsForType(datarow, 'fblikes', datestouse[i], sources);
+	  //datarow = addStatsForType(datarow, 'fbshares', datestouse[i], sources);
+	  //datarow = addStatsForType(datarow, 'fbcomments', datestouse[i], sources);
+	  //datarow = addStatsForType(datarow, 'shareratio', datestouse[i], sources);
+	  datarow["estimated"] = dayswithoutdata > 1 ? true : false;
+	  datarow["total_shares"] = parseInt(video_total_share[i] + blog_total_share[i]); 
+ 		datarow["num_shares"] = parseInt(datarow["total_shares"] - previousShares) / dayswithoutdata;  
+
+ 		if(!isNaN(datarow["num_shares"]))
+ 		maxShares = Math.max(datarow["num_shares"], maxShares); 
+
+ 		datarow["total_views"] = parseInt(video_total_ytviews[i]); 
+ 		datarow["num_views"] = parseInt(datarow["total_views"] - previousViews) / dayswithoutdata;
+
+ 		if(!isNaN(datarow["num_views"]))
+ 		maxViews = Math.max(datarow["num_views"], maxViews); 
+
+	  previousShares = datarow["total_shares"];
+	  previousViews = datarow["total_views"];
+	  //datarow.push("test");
+	  numDataLines = datarow.length;
+	  maindata.push(datarow);
+	  dayswithoutdata = 1;
+	}
+	
+	currentdate = firstdate;
+	currentdate.setDate(firstdate.getDate()+1);
+  }
+
+  	var returnData = Array();
+  	
+  	returnData["total_shares"] = data.totalstats.totalfb + data.totalstats.totaltweets;
+  	returnData["total_views"] = data.totalstats.totalviews;
+  	returnData["max_shares"] = maxShares;
+  	returnData["max_views"] = maxViews;
+	returnData["startdate"] = data.startdate;
+	returnData["client"] = data.client;
+	returnData["campaign"] = data.campaign;
+	
+	returnData["timeline"] = maindata;
+  	sources["this"].campaignLoaded(returnData);
+  
+  
+	
+
+	  });
+
+	  function union_arrays (x, y) {
+		var obj = {};
+		for (var i = x.length-1; i >= 0; -- i)
+		   obj[x[i]] = x[i];
+		for (var i = y.length-1; i >= 0; -- i)
+		   obj[y[i]] = y[i];
+		var res = []
+		for (var k in obj) {
+		  if (obj.hasOwnProperty(k))  // <-- optional
+			res.push(obj[k]);
+		}
+		return res;
+	  }
+
+
+  function addStatsForType(data, type, date, sources){
+	
+	if($('#stats_'+type).attr('checked')|| 1 == 1){
+	  
+	  var index_video = video_date_date.indexOf(date);
+	  var index_blog = blog_date_date.indexOf(date);
+	  
+	  
+	  var ontbrekendeblogdagen = days_between(String(blog_date_date[index_blog]), String(blog_date_date[index_blog-1]));
+	  var ontbrekendevideodagen = days_between(String(video_date_date[index_video]), String(video_date_date[index_video-1]));
+	
+	  if(type == "shareratio"){
+		if(index_video == -1 || index_blog == -1){
+		  data["shareratio"] = null;
+		} else {
+		
+		  var totalvideo = parseInt(sources["video_total_share"][index_video]);
+		  var totalblog = parseInt(sources["blog_total_share"][index_blog]);
+		  var totalview = parseInt(sources["video_total_ytviews"][index_video]);
+		  
+		  data["shareratio"] =(totalview / (totalvideo + totalblog));
+		  
+		  
+		}
+		return data;
+	  }
+	  
+
+	  if($("#show").val() == "video" || 1 == 1){
+		if(index_video == -1){
+		  data["video_" + type] = (null)
+		} else {
+		  data["video_" + type] = (parseInt(giveNumberOfTheDay(sources["video_total_" + type][index_video], sources["video_total_" + type][index_video-1], ontbrekendevideodagen)));
+		}
+	  }
+	  if($("#show").val() == "blog" || 1 == 1){
+		if(index_blog == -1){
+		  data["blog_"] = (null)
+		} else {
+		  if(sources["blog_total_" + type]){
+		  
+			data["blog_" + type] = (parseInt(giveNumberOfTheDay(sources["blog_total_" + type][index_blog], sources["blog_total_" + type][index_blog-1], ontbrekendeblogdagen)));
+		  } else {
+			data["blog_" + type] = (null);
+		  }
+		}
+	  }
+	}
+	
+	return returnData;
+  }
+
+  function days_between(date1_string, date2_string) {
+
+	  // The number of milliseconds in one day
+	  var ONE_DAY = 1000 * 60 * 60 * 24
+
+	  // Convert both dates to milliseconds
+	var date1_arr = date1_string.split("-");
+	var date1 = new Date(parseInt(date1_arr[0]), parseInt(date1_arr[1])-1, parseInt(date1_arr[2]));
+	var date2_arr = date2_string.split("-");
+	var date2 = new Date(parseInt(date2_arr[0]), parseInt(date2_arr[1])-1, parseInt(date2_arr[2]));
+	  
+	  
+	  var date1_ms = date1.getTime()
+	  var date2_ms = date2.getTime()
+
+	  // Calculate the difference in milliseconds
+	  var difference_ms = Math.abs(date1_ms - date2_ms)
+	  
+	  // Convert back to days and return
+	
+	  return Math.round(difference_ms/ONE_DAY)
+
+  }
+
+  function giveNumberOfTheDay(today, yesterday, aantaldagendieontbreken){
+
+	if($("#interval").val() == "daily"){
+	  if(yesterday == undefined){
+		return today
+	  }
+	  
+	  if(today - yesterday < 0){
+		
+		return null
+	  } 
+	  
+	  if(aantaldagendieontbreken > 0){
+		return (today - yesterday) / (aantaldagendieontbreken);
+	  } else {
+		return today - yesterday;
+	  }
+	  
+	} else {
+	  return today;
+	}
+  }
+
+
+};
+		
+
+
---
+++ assets/js/BuzzdashViz.js
@@ -0,0 +1,383 @@
+/*
+ *	Buzzdash Visualisation
+ *
+ *	v1.0 - given basic parameters, draw a responsive and animated chart with data from Buzzdash
+ *	
+ *	Feature roadmap:
+ *
+ *	- v1.1 - retina testing
+ *	- v1.2 - additional media-queries & sizes
+ *	- v2.0 - interactivity: hover on bars to see actual numbers
+ */
+
+function BuzzdashViz($el, options) {
+	this.$el = $el;
+	
+	this.setup(options);
+}
+
+BuzzdashViz.prototype = {
+	
+	api: null,			// reference to the BuzzdashAPI
+	
+	$el: null,			// reference to container element (jquery)
+	options: {			// options read from the data-* properties
+		campaignID: null,		// ID of the campaign-results to chart: get this value from inside Buzzdash
+		animated: true,			// is the chart animated?
+		animationDuration: 60,	// duration in frames
+		pixelRatio: 1,			// for dealing with retina, hi-dpi, etc...
+		resizable: true,		// is this static or liquid?
+		cansave: true			// can you right click>save on the visual? (toggles between attaching canvas & img)
+	},		
+	
+	$img: null, 		// container for the canvas data-url
+	canvas: null, 		// off-screen canvas for rendering
+	context: null,		// 2D Drawing context
+	
+	needsRender: false,	// view invalidation: set to true to re-draw
+	
+	data: null,			// JSON gotten from the BuzzdashAPI
+	stage: {			// stage for rendering	(logical nested objects)
+		
+		views: [],		// views-bars model
+		shares: [],		// shares-bars model
+		playhead: 0,	// for keeping track of the animation state
+		
+		width: 0,
+		height: 0,
+		prevwidth: -1,
+		prevheight: -1,
+		
+		marginw: 0,		// margins to center the chart on stage
+		marginh: 0,
+		
+		media: {	// "media queries" : different constants for different sizes
+			'small' :  {
+				minwidth: 0,
+				numbars: 10,
+				barwidth: 8,
+				bargap: 2
+			},
+			'medium' : {
+				minwidth: 200,
+				numbars: 20,
+				barwidth: 8,
+				bargap: 2
+			},
+			'large' : {
+				minwidth: 400,
+				numbars: 20,
+				barwidth: 16,
+				bargap: 4
+			},
+			'hueg' : {
+				minwidth: 800,
+				numbars: 32,
+				barwidth: 20,
+				bargap: 5
+			},
+			'potat0wned' : {
+				minwidth: 1200,
+				numbars: 42,
+				barwidth: 20,
+				bargap: 5
+			}
+		},
+		selectedmedia: 'small',	// active "media query"	id
+		prevmedia: null,		// previously active "media query" id
+		
+		views_color: '#ff5d3b',
+		shares_color: '#ffffff'
+	},
+	
+	setup: function (options) {		// setup/constructor method
+		var $ref = this;
+		
+		this.api = new BuzzdashAPI();
+		
+		if(window.devicePixelRatio !== undefined) {
+			this.options.pixelRatio = window.devicePixelRatio;
+		}
+		
+		if(options != null) {
+			$.extend(this.options, options);
+		} else {
+			var dataOptions = {};
+		
+			if($ref.$el.data("animation-duration") !== undefined) {
+				dataOptions["animationDuration"] = $ref.$el.data("animation-duration");
+			} 
+			
+			if($ref.$el.data("campaign-id") !== undefined) {
+				dataOptions["campaignID"] = $ref.$el.data("campaign-id");
+			} 
+			
+			if($ref.$el.data("animated") !== undefined) {
+				dataOptions["animated"] = $ref.$el.data("animated");
+			}
+			
+			if($ref.$el.data("resizable") !== undefined) {
+				dataOptions["resizable"] = $ref.$el.data("resizable");
+			}
+			
+			if($ref.$el.data("can-save") !== undefined) {
+				dataOptions["cansave"] = $ref.$el.data("can-save");
+			}
+			
+			if($ref.$el.data("pixel-ratio") !== undefined) {
+				dataOptions["pixelRatio"] = $ref.$el.data("pixel-ratio");
+			}
+			
+			$.extend(this.options, dataOptions);
+		}
+				
+		if(this.options.campaignID == null || this.options.campaignID == undefined) {
+			alert("Error: can't graph results, no campaign ID was provided");
+			
+			return;
+		}
+				
+		if(this.options.resizable) {
+			$(window).resize(function() {
+				$ref.measure();
+				
+				if($ref.stage.prevwidth !== $ref.stage.width || $ref.stage.prevheight !== $ref.stage.height) {
+					$ref.$img.attr("width", $ref.stage.width/$ref.options.pixelRatio);
+					$ref.$img.attr("height", $ref.stage.height/$ref.options.pixelRatio);
+					
+					$($ref.canvas).attr("width", $ref.stage.width/$ref.options.pixelRatio);
+					$($ref.canvas).attr("height", $ref.stage.height/$ref.options.pixelRatio);
+					
+					$ref.stage.playhead = 0;					
+					$ref.needsRender = true;
+				}
+			});
+		}
+		this.measure();
+		
+		this.createMarkup(this.$el);
+		
+		// get data
+		this.api.campaignLoaded = function(data) {
+			$ref.campaignLoaded(data);	
+		};
+		this.api.loadCampaign(this.options.campaignID);
+		
+		// requestanimationframe - setup render loop
+		(function animLoop() {
+			requestAnimationFrame(animLoop);
+			if($ref.needsRender) {
+				$ref.render();
+			}
+		})();
+	},
+	
+	render: function() {
+		// local vars
+		var $ref = this,
+			stage = this.stage,
+			data = this.data,
+			options = this.options,
+			ctx = this.context,
+			views = this.stage.views,
+			shares = this.stage.shares,
+			mq = this.stage.media[stage.selectedmedia],
+			
+			numbars,
+			barwidth,
+			bargap,
+			marginw = 0,
+			marginh = 0,
+			scale,
+			max_views = 0,
+			max_shares = 0;
+		
+		numbars = mq.numbars;
+		barwidth = mq.barwidth * options.pixelRatio;
+		bargap = mq.bargap * options.pixelRatio;
+				
+		// scale
+		scale = (((stage.height - 10)/2) / data.max_views)/2;
+		
+		
+		// generate data & calculate margins
+		if(views.length == 0 && mq != null) {
+			var timeline,
+				data_per_bar,
+				counter = 0;
+			
+			data_per_bar = Math.round(data.timeline.length / numbars);
+			
+			// combine 
+			for(var i = 0; i < data.timeline.length; i++) {
+				timeline = data.timeline[i];
+				
+				if(counter%data_per_bar == 0) {
+					views.push({
+						count: !isNaN(timeline.num_views)? Math.max(timeline.num_views, 0) : 0,
+						anim: 0,
+						date: timeline.date
+					});
+					shares.push({
+						count: !isNaN(timeline.num_shares)? Math.max(timeline.num_shares, 0) : 0,
+						anim: 0,
+						date: timeline.date
+					});
+				} else {
+					views[views.length - 1].count += !isNaN(timeline.num_views)? Math.max(timeline.num_views, 0) : 0;
+					shares[shares.length - 1].count += !isNaN(timeline.num_shares)? Math.max(timeline.num_shares, 0) : 0; 
+				}
+				
+				if(views[views.length - 1].count > max_views) {
+					max_views = views[views.length - 1].count;
+				}
+				
+				if(shares[shares.length - 1].count > max_shares) {
+					max_shares = shares[shares.length - 1].count;
+				}
+				
+				counter ++;
+			}
+			
+			// margins
+			stage.marginw = (stage.width - (views.length * (barwidth + bargap)))/2;
+			stage.marginh = (stage.height - ((max_views + max_shares)*scale + bargap))/2;
+
+		}
+		
+		// draw bargraphs
+		
+		// clear 
+		ctx.clearRect(0, 0, stage.width, stage.height);
+		
+		for(var i = 0; i < views.length; i++) {
+			var view = views[i], 
+				share = shares[i];
+						
+			if(view !== undefined) {
+				if(options.animated) {
+					view.anim = share.anim = Math.min(Math.ceil(options.animationDuration/numbars), Math.max(0, stage.playhead - i)); //Math.max(0, Math.min(options.animationDuration, stage.playhead*(views.length - i))); 
+				} else {
+					view.anim = share.anim = Math.max(1, Math.ceil(options.animationDuration/numbars));
+				}
+			
+				var view_height = Math.max(Math.round(view.count*scale), 1) * (view.anim / Math.ceil(options.animationDuration/numbars)),
+					share_height = Math.max(Math.round(share.count*scale), 1) * (share.anim / Math.ceil(options.animationDuration/numbars)),
+					xoffset = i * (barwidth + bargap) + stage.marginw,
+					yoffset_views = Math.round(((stage.height)/2) - view_height + stage.marginh/2),
+					yoffset_shares = Math.round(((stage.height)/2) + bargap + stage.marginh/2);
+					
+				ctx.fillStyle = stage.views_color;
+				ctx.fillRect(xoffset, yoffset_views, barwidth, view_height);
+				
+				ctx.fillStyle = stage.shares_color;
+				ctx.fillRect(xoffset, yoffset_shares, barwidth, share_height);
+			} 
+		}
+		
+		// advance playhead
+		if(options.animated) {
+			stage.playhead ++;
+			
+			if(stage.playhead == options.animationDuration) {
+				$ref.needsRender = false;
+			}
+		}
+		
+		
+		$ref.$img.attr('src', $ref.canvas.toDataURL());
+	},
+	
+	createMarkup: function($el) {
+		$el.empty();
+		
+		this.canvas = $('<canvas/>', { Width: this.stage.width, Height: this.stage.height})[0];
+		this.$img = $('<img />', {Width: this.stage.width / this.options.pixelRatio, Height: this.stage.height/this.options.pixelRatio});
+		
+		this.context = this.canvas.getContext('2d');
+				
+		if(this.options.cansave){		
+			$el.append(this.$img);
+		} else {
+			$el.append(this.canvas);
+		}
+	},
+	
+	measure: function () {	
+		if(this.$el) {
+			this.stage.prevwidth = this.stage.width;
+			this.stage.prevheight = this.stage.height;
+			
+			this.stage.width = this.$el.width() * this.options.pixelRatio;
+			this.stage.height = this.$el.height() * this.options.pixelRatio;
+			
+			if(this.stage.width != this.stage.prevwidth || this.stage.height != this.stage.prevheight) {
+				// store previous query
+				this.stage.prevmedia = this.stage.selectedmedia;
+								
+				// check which media query is active
+				for(var id in this.stage.media) {
+					var mq = this.stage.media[id];
+										
+					if (this.stage.width > mq.minwidth) {
+						this.stage.selectedmedia = id;
+					}
+				}
+				
+				// empty arrays -> this causes a re-calculation of the data-models
+				this.stage.views = [];
+				this.stage.shares = [];
+			}
+			
+		}
+	},
+	
+	campaignLoaded: function (data) {	// JSON is loaded from API
+		this.data = data;
+		
+		this.needsRender = true;
+	},
+	
+	random: function(min, max) {	// random number helper method
+		if(min > max) {
+			var sanity = min;
+			min = max;
+			max = sanity;
+		}
+		
+		return min + Math.random()*(max - min);
+	},
+	
+	lerp: function(start, end, delta) {	// linear interpolation helper method
+		var diff = (end - start);
+		
+		return (start + diff*delta);
+	}
+};
+
+
+// Request Animation Frame Polyfill (source: http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/)
+(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
